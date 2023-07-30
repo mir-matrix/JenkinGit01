588f021fe5d47e4ee37a80ec1543739f3c8f6b91
miraclematrix.dj <miraclematrix.dj@gmail.com>
2023-07-30 12:42:30 +0530

    first commit

---
+++ src/main/java/snippet/Snippet.java
@@ -0,0 +1,8 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		C:\VcentryBatch\Batch261\I1_GitTest
+	}
+}
+
---
+++ src/test/java/a1_googleTest/A1_GoogleTest.java
@@ -0,0 +1,35 @@
+package a1_googleTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class A1_GoogleTest {
+
+	@Test
+	public void validateGoogleTest() throws IOException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver wd = new ChromeDriver();
+		wd.manage().window().maximize();
+		wd.get("https://www.google.com/");
+		System.out.println(wd.getTitle());
+		
+		wd.findElement(By.name("q")).sendKeys("Chennai");
+		wd.findElement(By.name("q")).submit();
+		System.out.println(wd.getTitle());
+		
+		
+		File src = ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+	FileHandler.copy(src, new File("C:\\VcentryBatch\\Batch261\\I1_GitTest\\Screenshot\\google.png"));
+	wd.quit();
+	}
+}
